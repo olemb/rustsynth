3773b5e7e3111c532089ed39acf63035a7f072d6
Ole Martin Bjørndalen <ombdalen@gmail.com>
2020-12-20 13:11:47 +0100

    Ran Clippy and fixed a thing.

--- src/audiofile.rs
+++ src/audiofile.rs
@@ -7,7 +7,7 @@ fn encode_sample(sample: &f64) -> i16 {
     (sample * 32000.0) as i16
 }
 
-pub fn save_samples(filename: &str, samples: &Vec<f64>) {
+pub fn save_samples(filename: &str, samples: &[f64]) {
     let sample_rate = 44100;
     let file = File::create(&filename.to_string()).unwrap();
     let writer = BufWriter::new(file);
